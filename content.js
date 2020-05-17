59f6e01ea7d6bc7dc1d031348499b3ea89ee3991
noopy <young_ahn@yonsei.ac.kr>
2020-05-17 22:33:57 +0900

    update word selector for content in chrome window

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
 // select content script and send it to background.js
+console.log("content selector running");
+
+window.addEventListener("mouseup", selectWord);
+
+function selectWord() {
+  console.log("word selected");
+  let selectedText = window.getSelection();
+  console.log(selectedText);
+}
